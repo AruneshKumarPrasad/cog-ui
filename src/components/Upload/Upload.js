12103f466ef853fc4c7622609c0ef6b9de89f02e
ShivPrashant777 <shivprashant777@gmail.com>
2022-08-27 23:37:53 +0530

    some styles

--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -1,119 +1,141 @@
-import React, { useState } from "react";
-import Papa from "papaparse";
-import styles from "./Upload.module.css";
-import { Link, useNavigate } from "react-router-dom";
-import { auth, db } from "../../firebase";
-import { doc, setDoc, collection, query, where } from "firebase/firestore";
+import React, { useState } from 'react';
+import Papa from 'papaparse';
+import styles from './Upload.module.css';
+import { Link, useNavigate } from 'react-router-dom';
+import { auth, db } from '../../firebase';
+import { doc, setDoc, collection, query, where } from 'firebase/firestore';
+import Nav from '../Nav/Nav';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeftLong } from '@fortawesome/free-solid-svg-icons';
 
 // Allowed extensions for input file
-const allowedExtensions = ["csv"];
-
+const allowedExtensions = ['csv'];
 
 const Upload = () => {
+	const navigate = useNavigate();
+	// It state will contain the error when
+	// correct file extension is not used
+	const [error, setError] = useState('');
 
-    const navigate = useNavigate();
-    // It state will contain the error when
-    // correct file extension is not used
-    const [error, setError] = useState("");
+	// It will store the file uploaded by the user
+	const [file, setFile] = useState('');
 
-    // It will store the file uploaded by the user
-    const [file, setFile] = useState("");
+	const [nameOfFile, setName] = useState('');
 
-    const [nameOfFile, setName] = useState("");
+	const [date, setDate] = useState('');
 
-    const [date, setDate] = useState("");
+	const [time, setTime] = useState('');
 
-    const [time, setTime] = useState("");
+	var today = new Date();
 
-    var today = new Date();
-    
-    // This function will be called when
-    // the file input changes
-    const handleFileChange = (e) => {
-        setError("");
+	// This function will be called when
+	// the file input changes
+	const handleFileChange = (e) => {
+		setError('');
 
-        // Check if user has entered the file
-        if (e.target.files.length) {
-            const inputFile = e.target.files[0];
-            // Check the file extensions, if it not
-            // included in the allowed extensions
-            // we show the error
-            const fileExtension = inputFile?.type.split("/")[1];
-            if (!allowedExtensions.includes(fileExtension)) {
-                setError("Please input a csv file");
-                return;
-            }
+		// Check if user has entered the file
+		if (e.target.files.length) {
+			const inputFile = e.target.files[0];
+			// Check the file extensions, if it not
+			// included in the allowed extensions
+			// we show the error
+			const fileExtension = inputFile?.type.split('/')[1];
+			if (!allowedExtensions.includes(fileExtension)) {
+				setError('Please input a csv file');
+				return;
+			}
 
-            // If input type is correct set the state
-            const dateInstance = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-            const timeInstance = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
-            const nameFile = inputFile.name;
-            setDate(dateInstance);
-            setTime(timeInstance);
-            setName(nameFile);
-            setFile(inputFile);
-        }
-    };
-    
-    const handleParse = () => {
+			// If input type is correct set the state
+			const dateInstance =
+				today.getFullYear() +
+				'-' +
+				(today.getMonth() + 1) +
+				'-' +
+				today.getDate();
+			const timeInstance =
+				today.getHours() +
+				':' +
+				today.getMinutes() +
+				':' +
+				today.getSeconds();
+			const nameFile = inputFile.name;
+			setDate(dateInstance);
+			setTime(timeInstance);
+			setName(nameFile);
+			setFile(inputFile);
+		}
+	};
 
-        // If user clicks the parse button without
-        // a file we show a error
-        if (!file) return setError("Enter a valid file");
+	const handleParse = () => {
+		// If user clicks the parse button without
+		// a file we show a error
+		if (!file) return setError('Enter a valid file');
 
-        // Initialize a reader which allows user
-        // to read any file or blob.
-        const reader = new FileReader();
-        // Event listener on reader when the file
-        // loads, we parse it and set the data.
-        reader.onload = async ({ target }) => {
-            const csv = Papa.parse(target.result, { header: true });
-            const parsedData = csv?.data;
-            const columns = Object.keys(parsedData[0]);
-            const docRef= doc(db, "users", auth.currentUser.uid, "documents", nameOfFile)
-            await setDoc(docRef, {
-                date : date,
-                time : time,
-                uploader : auth.currentUser.displayName,
-                array : parsedData,
-                keys : columns,
-                name : nameOfFile
-            } , { merge: true });
-        };
-        reader.readAsText(file);
-        navigate("/landingpage");
+		// Initialize a reader which allows user
+		// to read any file or blob.
+		const reader = new FileReader();
+		// Event listener on reader when the file
+		// loads, we parse it and set the data.
+		reader.onload = async ({ target }) => {
+			const csv = Papa.parse(target.result, { header: true });
+			const parsedData = csv?.data;
+			const columns = Object.keys(parsedData[0]);
+			const docRef = doc(
+				db,
+				'users',
+				auth.currentUser.uid,
+				'documents',
+				nameOfFile
+			);
+			await setDoc(
+				docRef,
+				{
+					date: date,
+					time: time,
+					uploader: auth.currentUser.displayName,
+					array: parsedData,
+					keys: columns,
+					name: nameOfFile,
+				},
+				{ merge: true }
+			);
+		};
+		reader.readAsText(file);
+		navigate('/landingpage');
+	};
 
-    };
-
-    return (
-        <div className={styles.container}>
-        <div className={styles.innerBox}>
-            <h1>
-            <Link to="/landingpage">
-                LandingPage
-            </Link>
-            </h1>
-            <h1>
-                <Link to="/listpage">ListPage</Link>
-                </h1>
-                <label htmlFor="csvInput" style={{ display: "block" }}>
-                    Enter CSV File
-                </label>
-                <input
-                onChange={handleFileChange}
-                id="csvInput"
-                name="file"
-                type="File"
-            />
-            <div>
-                <button onClick={handleParse}>Parse</button>
-            </div>
-            <div style={{ marginTop: "3rem" }}>
-                {error ? error :  ""}
-            </div>
-            </div>
-        </div>
-    );
+	return (
+		<div className={styles.container}>
+			<Nav />
+			<h1 className={styles.backbtn}>
+				<Link to="/landingpage">
+					{' '}
+					<FontAwesomeIcon icon={faArrowLeftLong} />
+				</Link>
+			</h1>
+			<div className={styles.innerBox}>
+				<label htmlFor="csvInput" style={{ display: 'block' }}>
+					<h3>Select a file to upload</h3>
+				</label>
+				<div className={styles.filecontainer}>
+					<input
+						onChange={handleFileChange}
+						id="csvInput"
+						name="file"
+						type="File"
+					/>
+				</div>
+				<div>
+					<button onClick={handleParse}>Upload</button>
+				</div>
+				{error && (
+					<div style={{ marginTop: '3rem' }}>
+						{error ? error : ''}
+					</div>
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default Upload;
