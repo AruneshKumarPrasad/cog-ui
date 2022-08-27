0857aa2793bd20a5bc27040eb809eb75486e7d5f
AruneshKumarPrasad <aruneshkumarprasad@gmail.com>
2022-08-28 01:20:06 +0530

    missed props

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { collection, query, getDocs } from 'firebase/firestore';
 
-function LandingPage(props) {
+function LandingPage() {
   const [arrayOfDocs, setArrayOfDocs] = useState([]);
 
 	const handleCheckList = async () => {
