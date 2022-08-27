f360c46e8ff49a3f901a7c5676d0d217e73d332a
AruneshKumarPrasad <aruneshkumarprasad@gmail.com>
2022-08-28 01:09:42 +0530

    Final Name List

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { collection, query, getDocs } from 'firebase/firestore';
 
-function LandingPage(props) {
+function LandingPage() {
   const [arrayOfDocs, setArrayOfDocs] = useState([]);
 
 	const handleCheckList = async () => {
@@ -38,7 +38,7 @@ function LandingPage(props) {
 										<Link
 											to="/listpage"
 											state= {{ docID: doc.id }}>
-											{index+1} {doc.data()['name']} {doc.data()['time']} {doc.data()['array'].length}
+											{index+1}  Name:{doc.data()['name']}  Created-on: {doc.data()['date']} at {doc.data()['time']}  Records: {doc.data()['array'].length}
 										</Link>
 									</li>
 								);
